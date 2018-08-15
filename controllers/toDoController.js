f83a5e5fa14ed7deb62f0fcc995e50b0a774ef2c
MiguelCabailo <mcliguelcabailo12@gmail.com>
2018-08-15 17:57:58 +1000

    connected and added an item to a mongoDB database

--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -1,6 +1,27 @@
 var bodyParser = require('body-parser');
-var urlEncodedParser = bodyParser.urlencoded({extended:false});
+var mongoose = require('mongoose');
 
+// connect to the DB
+mongoose.connect('mongodb://root:toor102938@ds018508.mlab.com:18508/todoapplearning'
+, { useNewUrlParser: true });
+
+// schema for the DB(blue-print for the data)
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+
+// create a todo model and base it on a schema
+// Capital
+// parameters: Model Name stored as a collection/schema 
+var Todo = mongoose.model('Todo', todoSchema);
+
+// test dummy data
+var itemOne = Todo({item: 'buy flowers'}).save((err)=>{
+  if(err) throw err;
+  console.log('item saved');
+});
+
+var urlEncodedParser = bodyParser.urlencoded({extended:false});
 var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
 
 module.exports = (app)=>{
