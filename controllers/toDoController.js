f1edc44aa3b4bad198bfcc2acc66ea17bbe2058a
MiguelCabailo <mcliguelcabailo12@gmail.com>
2018-08-15 14:43:01 +1000

    added a post and delete ajax call to the server

--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -1,14 +1,29 @@
+var bodyParser = require('body-parser');
+var urlEncodedParser = bodyParser.urlencoded({extended:false});
+
+var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
+
 module.exports = (app)=>{
 
   app.get('/todo', (req,res)=>{
-    res.render('todo');
+    res.render('todo',{todos: data});
   });
 
-  app.post('/todo', (req,res)=>{
-
+  app.post('/todo', urlEncodedParser, (req,res)=>{
+    data.push(req.body);
+    // send data back once the data array has been updated
+    res.json(data);
   });
 
-  app.delete('/todo', (req,res)=>{
+  app.delete('/todo/:item', (req,res)=>{
+    // filter out item to be deleted
+    // returns true if the particular item is not equal to the parameters passed in
+    // if the parameter matches the item inside the array it returns false and takes it out
+    // filters it out, essentially deleting it
+    data = data.filter(function(todo){
+      return todo.item.replace(/ /g, '-') !== req.params.item;
+    });
 
+    res.json(data);
   });
 }
--- public/assets/todo-list.js
+++ public/assets/todo-list.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
 
   $('form').on('submit', function(){
-
+    
       var item = $('form input');
+      // todo is equal to the user input on the item form
       var todo = {item: item.val()};
 
       $.ajax({
@@ -14,15 +15,18 @@ $(document).ready(function(){
           location.reload();
         }
       });
-
+      
       return false;
-
+  
   });
 
+  // delete
   $('li').on('click', function(){
+    // replace empty spaces with - to match the query string
       var item = $(this).text().replace(/ /g, "-");
       $.ajax({
         type: 'DELETE',
+        //item replaced empty spaces sent to the url
         url: '/todo/' + item,
         success: function(data){
           //do something with the data via front-end framework
