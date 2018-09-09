6c0341175403f57fabee12ccadc9e5b347034d64
MiguelCabailo <mcliguelcabailo12@gmail.com>
2018-09-09 14:56:41 +1000

    modification for redux devtools compatible with heroku

--- client/src/store.js
+++ client/src/store.js
@@ -7,16 +7,13 @@ const initialState = {};
 // all middleware here and spread to separate them each on their own
 const middleware = [thunk];
 
-// (rootReducer, initialState, applyMiddleware)
-// compose is due to redux tools so we wrap middleware there
-const store = createStore(
-    rootReducer,
-    //optional
-    initialState, 
-    compose(
-        applyMiddleware(...middleware),
-        //copy paste it to be able to use the redux dev tools on chrome
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+const composeEnhancers =  typeof window === 'object' &&  window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+const composingMiddlewareAndDevTools = composeEnhancers(applyMiddleware(...middleware));
+
+const store = createStore(rootReducer, initialState, composingMiddlewareAndDevTools);
+
+console.log(store.getState());
 
 export default store;
