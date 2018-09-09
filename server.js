21e42d81cd7056527a26063e6821fed177ede7e5
MiguelCabailo <mcliguelcabailo12@gmail.com>
2018-09-09 16:04:42 +1000

    server.js

--- server.js
+++ server.js
@@ -33,10 +33,14 @@ if(process.env.NODE_ENV === 'production'){
 
     app.use(express.static('client/build'));
 
+    app.get('/', (req,res)=>{
+        res.sendfile(__dirname + '/client/build/index.html');
+    })
+
     // all requests load index.html
     app.get('*', (req,res)=>{
         // should load the html unless hitting the api
-        res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.redirect('/');
     })
 }
 
